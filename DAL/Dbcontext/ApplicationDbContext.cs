c4d1a580958d706a1d412ad3703655a543f602b6
admin <admin@DESKTOP-JKD834T>
2025-04-25 11:26:01 +0200

    Add project files.

---
+++ DAL/Dbcontext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Dbcontext
+{
+   public  class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+    
+
+
+
+    }
+}
